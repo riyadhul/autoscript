c9b7e51356d3d952f0056efc315642089acf115a
riyadhul <riyadhul@gmail.com>
2021-08-23 03:17:57 +0700

    arya
    aryacarbon

---
+++ about.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#       This Script Edited By AryaCarbon        #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By AryaCarbon                        #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Allah YME                                     #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# Admin Portal & White Vps                      #"
+echo -e "# aryacarbon.my.id                              #"
+echo -e "# AryaCarbon                                    #"          #"
+echo -e "# Allah                                         #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
